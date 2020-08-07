94e0cdcd9d158e525520fe39abf203ad7e54fbd8
azalia17 <azaliaaps17@gmail.com>
2020-08-07 17:02:46 +0700

    ini endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+        return "NIS : 3103118030 
+        Nama : Azalia Amanda Putri Sampurno 
+        Gender: Perempuan 
+        Phone : 08157918200
+        Kelas: XII RPL 1";
+    }
+}
